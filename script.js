6cf339d5a8e36ec7a62f637f40253cb17fc027ba
antonioalgmar <antonioalgmar@gmail.com>
2023-06-13 00:49:26 -0300

    mudanca de profile no mode light

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
 function toggleMode () {
     const html = document.documentElement
-    
-    //if(html.classList.contains("light")){
-    //    html.classList.remove("light")
-    //} else {
-    //    html.classList.add("light")
-    //}
-
     html.classList.toggle("light")
-}
+
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+    //se tiver light mode, adicionar a imagem avatar1
+    if(html.classList.contains("light")){
+        //substituir imagem
+        img.setAttribute("src", "./assets/avatar1.jpg")
+    } else {
+        //se tiver sem light
+        img.setAttribute("src", "./assets/avatar.jpg")
+    }
+    
+
+    
+}   
